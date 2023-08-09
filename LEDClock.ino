090bdda1171599aea7dfa7da4dca602ae34cdff9
Zawad Dewan <zawaddewan@gmail.com>
2023-08-09 15:16:59 -0400

    shionevenno

--- LEDClock.ino
+++ LEDClock.ino
@@ -2,11 +2,19 @@
 #include <TimeLib.h>
 
 #define LED_PIN     6
-#define NUM_LEDS    144
-#define BRIGHTNESS  255
+#define START_LED   24
+#define END_LED     142
+#define BRIGHTNESS  32
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
+#define NUM_LEDS END_LED - START_LED + 1
+
 CRGB leds[NUM_LEDS];
+
+
+
+int ledsPerMinute = (NUM_LEDS) / 60;
+int ledsPerHour = (NUM_LEDS) / 12;
 
 #define UPDATES_PER_SECOND 1
 
@@ -25,21 +33,37 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   // Turn the LED on, then pause
-  for (int i = 0 ; i < NUM_LEDS ; i++) {
-    if (i == second()) {
-      leds[i] = CRGB::Blue;
-    }
-    else if (i == minute()) {
+  // for (int i = 0 ; i < NUM_LEDS ; i++) {
+  //   if (i == second()) {
+  //     leds[i] = CRGB::Blue;
+  //   }
+  //   else if (i == minute()) {
+  //     leds[i] = CRGB::White;
+  //   } else if (i == hour() * 5) {
+  //     leds[i] = CRGB::Green;
+  //   }
+  //   else {
+  //     leds[i] = CRGB::Black;
+  //   }
+  // }
+  int minLed = int(minute() * ledsPerMinute) % NUM_LEDS + START_LED;
+  int hourLed = int(hour() * ledsPerHour) % NUM_LEDS + START_LED;
+
+  for(int i = START_LED; i <= END_LED; i++) {
+    if(i == minLed || i == hourLed) {
       leds[i] = CRGB::White;
-    } else if (i == hour() * 5) {
-      leds[i] = CRGB::Green;
-    }
-    else {
+    }else {
       leds[i] = CRGB::Black;
     }
   }
+  Serial.print("daniel haokun patosai steven xu");
+  Serial.print(hour());
+  Serial.print(hourLed);
+  Serial.print(minLed);
+  
 
   FastLED.show();
+  
   delay(1000 / UPDATES_PER_SECOND);
 }
 
