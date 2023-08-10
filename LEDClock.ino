cce745e6cd6aeddebaab9d9a34f211c131714e5b
pyshrekek <rukereero@gmail.com>
2023-08-10 13:50:08 -0400

    zawab dewan 2

--- LEDClock.ino
+++ LEDClock.ino
@@ -2,31 +2,32 @@
 #include <TimeLib.h>
 #include <NewPing.h>
 
-#define LED_PIN     6
+#define LED_PIN     3
 #define START_LED   24
 #define END_LED     142
-#define TRIG_PIN    9
-#define ECHO_PIN    10
 #define BRIGHTNESS  32
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
 #define CLOCK_SIZE  (END_LED - START_LED + 1)
 #define NUM_LEDS    288
-#define MAX_DIST    400
 
 CRGB leds[NUM_LEDS];
-
-NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DIST);
-
-
-
 double ledsPerMinute = CLOCK_SIZE / 60.0;
 double ledsPerHour = CLOCK_SIZE / 12.0;
 
-#define UPDATES_PER_SECOND 1
+#define NUM_SENSORS 2
+#define MAX_DISTANCE 40
+int TRIG_PINS[NUM_SENSORS] = {5, 9};
+int ECHO_PINS[NUM_SENSORS] = {6, 10};
+NewPing sonar[NUM_SENSORS] = {
+  NewPing(TRIG_PINS[0], ECHO_PINS[0], MAX_DISTANCE),
+  NewPing(TRIG_PINS[1], ECHO_PINS[1], MAX_DISTANCE)
+};
+int distances[NUM_SENSORS];
+
+#define UPDATES_PER_SECOND 20
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 
   delay(3000);
@@ -38,29 +39,16 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // Turn the LED on, then pause
-  // for (int i = 0 ; i < NUM_LEDS ; i++) {
-  //   if (i == second()) {
-  //     leds[i] = CRGB::Blue;
-  //   }
-  //   else if (i == minute()) {
-  //     leds[i] = CRGB::White;
-  //   } else if (i == hour() * 5) {
-  //     leds[i] = CRGB::Green;
-  //   }
-  //   else {
-  //     leds[i] = CRGB::Black;
-  //   }
-  // }
+  for (int i = 0 ; i < NUM_SENSORS ; i++) {
+    distances[i] = sonar[i].ping_cm();
+  }
+  Serial.println(distances[0]);
+  Serial.println(distances[1]);
+
   int minLed = int(30 * ledsPerMinute) % CLOCK_SIZE + START_LED;
   int hourLed = int((3 * ledsPerHour)) % CLOCK_SIZE + START_LED;
-
-  int distance = sonar.ping_cm();
-  Serial.println(distance);
   
-  if(distance <= 20) {
-    Serial.println("on");
+  if(distances[0] <= 20 || distances[1] <= 20) {
     for(int i = START_LED; i <= END_LED; i++) {
       if(i == minLed) {
         leds[i] = CRGB::Green;
@@ -78,11 +66,10 @@ void loop() {
 
   leds[END_LED] = CRGB::Red;
   leds[START_LED] = CRGB::Blue;
-  Serial.println(minute());
+  // Serial.println(minute());
   
 
   FastLED.show();
-  // digitalClockDisplay();
   delay(1000 / UPDATES_PER_SECOND);
 }
 
