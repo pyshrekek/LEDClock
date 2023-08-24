652aa1e6ef735aface9aa391c9d628ec192526c3
pyshrekek <rukereero@gmail.com>
2023-08-24 14:01:42 -0400

    final before i left

--- LEDClock.ino
+++ LEDClock.ino
@@ -2,35 +2,37 @@
 #include <TimeLib.h>
 #include <NewPing.h>
 
-#define LED_PIN     3
-#define START_LED   24
-#define END_LED     142
-#define BRIGHTNESS  32
-#define LED_TYPE    WS2812
+#define LED_PIN 3
+#define START_LED 0
+#define END_LED 180
+#define BRIGHTNESS 32
+#define LED_TYPE WS2812
 #define COLOR_ORDER GRB
-#define CLOCK_SIZE  (END_LED - START_LED + 1)
-#define NUM_LEDS    288
+#define NUM_LEDS 180
 
 CRGB leds[NUM_LEDS];
-double ledsPerMinute = CLOCK_SIZE / 60.0;
-double ledsPerHour = CLOCK_SIZE / 12.0;
 
 #define NUM_SENSORS 2
 #define MAX_DISTANCE 40
-int TRIG_PINS[NUM_SENSORS] = {5, 9};
-int ECHO_PINS[NUM_SENSORS] = {6, 10};
+int TRIG_PINS[NUM_SENSORS] = { 5, 9 };
+int ECHO_PINS[NUM_SENSORS] = { 6, 10 };
 NewPing sonar[NUM_SENSORS] = {
   NewPing(TRIG_PINS[0], ECHO_PINS[0], MAX_DISTANCE),
   NewPing(TRIG_PINS[1], ECHO_PINS[1], MAX_DISTANCE)
 };
-int distances[NUM_SENSORS];
+int distances[2];
 
-#define UPDATES_PER_SECOND 20
+#define UPDATES_PER_SECOND 10
+
+int secondLEDS[3];
+int minuteLEDS[3];
+int hourLEDS[3];
 
 void setup() {
-  Serial.begin(9600);
+  delay(1000);
 
-  delay(3000);
+  Serial.begin(9600);
+  Serial.println("START");
 
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
@@ -39,58 +41,55 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0 ; i < NUM_SENSORS ; i++) {
-    distances[i] = sonar[i].ping_cm();
-  }
-  Serial.println(distances[0]);
-  Serial.println(distances[1]);
+  distances[0] = sonar[0].ping_cm();
+  distances[1] = sonar[1].ping_cm();
 
-  int minLed = int(30 * ledsPerMinute) % CLOCK_SIZE + START_LED;
-  int hourLed = int((3 * ledsPerHour)) % CLOCK_SIZE + START_LED;
-  
-  if(distances[0] <= 20 || distances[1] <= 20) {
-    for(int i = START_LED; i <= END_LED; i++) {
-      if(i == minLed) {
+  populate(secondLEDS, second() * 3);
+  populate(minuteLEDS, minute() * 3);
+  populate(hourLEDS, hour() * 3);
+
+  Serial.println("TEST");
+  if (distances[0] <= 25) {
+    for( int i = 0; i < NUM_LEDS; ++i) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+  }
+  else {
+  for (int i = START_LED; i < END_LED; i++) {
+      if (i == secondLEDS[0] || i == secondLEDS[1] || i == secondLEDS[2]) {
+        leds[i] = CRGB::White;
+      } else if (i == minuteLEDS[0] || i == minuteLEDS[1] || i == minuteLEDS[2]) {
+        leds[i] = CRGB::Red;
+      } else if (i == hourLEDS[0] || i == hourLEDS[1] || i == hourLEDS[2]) {
         leds[i] = CRGB::Green;
-      }else if (i == hourLed) {
-        leds[i] = CRGB::Purple;
-      }else {
+      } else {
         leds[i] = CRGB::Black;
       }
-    }
-  }else {
-    for(int i = START_LED; i <= END_LED; i++) {
-      leds[i] = CRGB::Black;
-    }
+    
+  }
   }
 
-  leds[END_LED] = CRGB::Red;
-  leds[START_LED] = CRGB::Blue;
-  // Serial.println(minute());
-  
+  Serial.println("test");
+  Serial.println(distances[0]);
+  Serial.println(distances[1]);
 
   FastLED.show();
   delay(1000 / UPDATES_PER_SECOND);
 }
 
-void digitalClockDisplay(){
-   // digital clock display of the time
-   Serial.print(hour());
-   printDigits(minute());
-   printDigits(second());
-   Serial.print(" ");
-   Serial.print(day());
-   Serial.print(" ");
-   Serial.print(month());
-   Serial.print(" ");
-   Serial.print(year());
-   Serial.println();
-}
 
-void printDigits(int digits) {
-   // utility function for digital clock display: prints preceding colon and leading 0
-   Serial.print(":");
-   if(digits < 10)
-      Serial.print('0');
-      Serial.print(digits);
+
+void populate(int array[], int val) {
+  array[0] = val - 1;
+  array[1] = val;
+  array[2] = val + 1;
+
+  for (int i = 0; i < 3; i++) {
+    if (array[i] == -1) {
+      array[i] = 180;
+    } else if (array[i] == 181) {
+      array[i] = 0;
+    }
+  }
 }
